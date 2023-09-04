6d581bc769cf9fd7814e422cebe60303352cfc6e
jjsdev97 <jjjs963@naver.com>
2023-09-04 10:52:24 +0900

    step2 - main 코드 정리

--- src/main/java/org/homework/Main.java
+++ src/main/java/org/homework/Main.java
@@ -2,9 +2,8 @@ package org.homework;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Todo todoList = new Todo();
         TodoManager todoManager = new TodoManager();
-        TodoUI todoUI = new TodoUI(todoList, todoManager);
+        TodoUI todoUI = new TodoUI(todoManager);
 
         todoUI.run();
     }
