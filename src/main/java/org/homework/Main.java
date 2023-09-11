724f0c2c32ead4734d938bf747aab77cd95b1db5
jjsdev97 <jjjs963@naver.com>
2023-09-11 14:16:29 +0900

    step3 - 정리

--- src/main/java/org/homework/Main.java
+++ src/main/java/org/homework/Main.java
@@ -1,7 +1,7 @@
 package org.homework;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         TodoManager todoManager = new TodoManager();
         InputView inputView = new InputView();
         TodoView todoView = new TodoView();
